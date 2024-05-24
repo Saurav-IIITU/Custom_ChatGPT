3ca0b06361a064f569f6358c5ab515c515d848d9
Saurav <91366385+Saurav-IIITU@users.noreply.github.com>
2024-05-24 13:41:56 +0530

    Update pdf_chatbot.py

--- pdf_chatbot.py
+++ pdf_chatbot.py
@@ -54,11 +54,11 @@ if uploaded_files:
         st.write("Reusing index...\n")
         vectorstore = Chroma(persist_directory="persist", embedding_function=OpenAIEmbeddings())
         index = VectorStoreIndexWrapper(vectorstore=vectorstore)
-    # else:
-    #     if PERSIST:
-    #         index = VectorstoreIndexCreator(vectorstore_kwargs={"persist_directory": "persist"}).from_loaders(loaders)
-    #     else:
-    #         index = VectorstoreIndexCreator().from_loaders(loaders)
+    else:
+        if PERSIST:
+            index = VectorstoreIndexCreator(vectorstore_kwargs={"persist_directory": "persist"}).from_loaders(loaders)
+        else:
+            index = VectorstoreIndexCreator().from_loaders(loaders)
     
     chain = ConversationalRetrievalChain.from_llm(
         llm=ChatOpenAI(model="gpt-3.5-turbo"),
