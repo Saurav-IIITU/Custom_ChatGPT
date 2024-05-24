10509b7f8a0f760d220e49ec45c396739ad4983a
Saurav <91366385+Saurav-IIITU@users.noreply.github.com>
2024-05-24 19:27:13 +0530

    Update pdf_chatbot.py

--- pdf_chatbot.py
+++ pdf_chatbot.py
@@ -58,8 +58,7 @@ if uploaded_files:
         if PERSIST:
             index = VectorstoreIndexCreator(vectorstore_kwargs={"persist_directory": "persist"}).from_loaders(loaders)
         else:
-            # index = VectorstoreIndexCreator().from_loaders(loaders)
-            print("ok")
+            index = VectorstoreIndexCreator().from_loaders(loaders)
     
     chain = ConversationalRetrievalChain.from_llm(
         llm=ChatOpenAI(model="gpt-3.5-turbo"),
